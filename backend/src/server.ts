6b076c2372dad298d72c30a075920aacc86295ce
Emersom <emersomnunes717@gmail.com>
2024-05-30 11:46:51 -0300

    add front-end in the project

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,14 +3,16 @@ import "express-async-errors";
 import { routes } from "./Routes";
 import { migrationsRun } from "./database/sqlite/migrations";
 import { AppError } from "./utils/AppError";
+import cors from "cors";
 
 migrationsRun();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use((error: AppError, request: Request, response: Response, next: NextFunction) => {
-  if(error instanceof AppError) {
+  if (error instanceof AppError) {
     return response.status(error.statusCode).json({
       status: "error",
       message: error.message
@@ -23,9 +25,9 @@ app.use((error: AppError, request: Request, response: Response, next: NextFuncti
     status: "error",
     message: "Internal server error"
   });
-}) 
+})
 
 app.use(routes);
 
-const PORT = 3000
+const PORT = 3333
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
